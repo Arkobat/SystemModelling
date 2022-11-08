2db76a15e847c0ece4350695463aa8208a3be693
Arkobat <kr-jo@live.dk>
2022-11-08 23:11:33 +0100

    Adds spacing between tests

--- src/Main.java
+++ src/Main.java
@@ -4,8 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
         new KolmogorovSmirnov(100).test();
+        System.out.println("\n\n");
         new ChiSq(10_000, 10).test();
+        System.out.println("\n\n");
         new Runs().test();
+        System.out.println("\n\n");
         new Autocorrelation().test();
     }
 
